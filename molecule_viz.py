3ce85c18e4cf604d60c7e760da8b50b6c5c42567
Jaize Miller <millerjaizemae@gmail.com>
2024-12-19 15:27:44 -0800

    Created the chemical reaction balancer py file. Added first third of functionality.

---
+++ molecule_viz.py
@@ -0,0 +1,24 @@
+#3d visualization of molecules
+
+#input a smile string or chem formula
+
+#display a 3d viz of the molecule
+#using rdkit and py3dmol libraries
+
+
+#helping me to practice learning new packages
+#chem informatics libraries
+#molecular geometries
+
+#optional spectral data analysis...
+#read in csv file using pandas (great for one liners)
+#signal processing using numpy
+    #transforming the data point into a numpy array
+    #use numpy array to find max and min in the array
+    #aka the peaks and troughs in a spectral line
+#plot the spectral lines
+    #using seaborn and matplotlib
+
+#optional chem kinetics simulator
+#chemical kinetics?? Chem Coder on youtube
+#measuring concentration of reactants and products over time
